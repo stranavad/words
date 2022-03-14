6edcc6875b52522fc157d8935996ff8654d56957
= <=>
2022-03-14 10:39:50 +0100

    1 added advanced menu with showGlobal toggle and watchers inside every word

--- components/Words.js
+++ components/Words.js
@@ -2,11 +2,11 @@ import { List, ListItem, ListItemText, IconButton } from "@mui/material";
 import { VisibilityOff, Visibility } from "@mui/icons-material";
 import Word from './Word';
  
-const WordsList = ({ words, deleteWord }) => {
+const WordsList = ({ words, deleteWord, showGlobal }) => {
 	return (
 		<List sx={{maxWidth: '400px', width: '100%'}}>
 			{words.map((word) => (
-                <Word key={word.id} word={word} deleteWord={deleteWord}/>
+                <Word key={word.id} word={word} deleteWord={deleteWord} showGlobal={showGlobal}/>
 			))}
 		</List>
 	);
