0bfa5ea986effb9255f3beafe5beac34988a3fd5
= <=>
2022-03-15 22:41:28 +0100

    scaling fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import AlertBar from "../components/AlertBar";
 import { useState } from "react";
 import { Stack } from "@mui/material";
+import Head from 'next/head';
 import '../styles.css';
 
 const App = ({ Component, pageProps }) => {
@@ -16,6 +17,12 @@ const App = ({ Component, pageProps }) => {
 
 	return (
 		<Stack alignItems="center">
+			<Head>
+				<meta
+					name="viewport"
+					content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi"
+				/>
+			</Head>
 			<Component {...pageProps} alert={alert} />
 			<AlertBar
 				text={alertText}
