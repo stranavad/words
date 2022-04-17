5641233728ce79761fdf0b523f3cb5af40774c2f
= <=>
2022-04-17 23:42:36 +0200

    73 created working learning beta

--- components/learning/Answers.jsx
+++ components/learning/Answers.jsx
@@ -4,7 +4,7 @@ const Answers = ({words, selectAnswer}) => {
     return (
         <Stack sx={{display: 'flex', flexDirection: 'row', flexWrap:'wrap', justifyContent: 'center', alignItems: 'center'}}>
             {words.map((word, key) => (
-                <Button key={key} onClick={() => selectAnswer(word)} variant="contained" sx={{ mr: 2, mb: 2}}>{word}</Button>
+                <Button key={key} onClick={() => selectAnswer(word)} variant="outlined" sx={{ mr: 2, mb: 2}}>{word}</Button>
             ))}
         </Stack>
     )
